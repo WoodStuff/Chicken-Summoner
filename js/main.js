6dca15d46eb89029d05b01f762e877332f58a2c1
WoodStuff <woodstuffthings@gmail.com>
2022-05-21 12:14:12 +0200

    movement

--- js/main.js
+++ js/main.js
@@ -5,21 +5,59 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width;
-const height = canvas.height;
+var width = canvas.width;
+var height = canvas.height;
 
+let l, r; // abbreviations for left and right
+
+// every frame, runs 60 fps, for technical stuff
 function tick() {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	width = canvas.width = window.innerWidth;
+	height = canvas.height = window.innerHeight;
+
+	if (l) player.x -= 0.005;
+	if (r) player.x += 0.005;
 }
 
-setInterval(tick, 10);
+setInterval(tick, 100/6);
 
-// every frame
+const player = {
+	x: 0.5,
+}
+
+// every frame, for rendering stuff
 function init() {
+	ctx.clearRect(0, 0, xtopixel(1), ytopixel(1));
+
 	// background
-	ctx.fillStyle = '#101080';
+	ctx.fillStyle = '#5050A0';
 	ctx.fillRect(0, 0, xtopixel(1), ytopixel(1));
+
+	const man = new Image();
+	man.src = 'media/images/man.png';
+
+	// edit this parts when editing y spawn point                  ---
+	ctx.drawImage(man, xtopixel(player.x - (0.075 / 2)), (ytopixel(0.68) - xtopixel(0.075 / 2)), xtopixel(0.075), xtopixel(0.075));
 
 	requestAnimationFrame(init);
 }
+
+function keyDownHandler(event) {
+	if(event.keyCode == 39) {
+		r = true;
+	}
+	else if(event.keyCode == 37) {
+		l = true;
+	}
+}
+
+function keyUpHandler(event) {
+	if (event.keyCode == 39) {
+		r = false;
+	}
+	else if (event.keyCode == 37) {
+		l = false;
+	}
+}
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
