b49baf1622ffea515465347dba1652f48aa0f897
WoodStuff <woodstuffthings@gmail.com>
2022-05-21 10:49:34 +0200

    the canvas now fills the whole screen

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*
+	the main code of the game, primarily drawing to the canvas
+*/
+
+var canvas = document.getElementById('game');
+var game = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
