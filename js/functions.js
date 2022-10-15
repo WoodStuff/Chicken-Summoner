d112dda03f6cf0587d4e8e8793285c5c01657f7c
WoodStuff <woodstuffthings@gmail.com>
2022-10-15 22:11:56 +0200

    starting the menu

--- js/functions.js
+++ js/functions.js
@@ -2,31 +2,45 @@
 	helper functions & stuff
 */
 
+/**
+ * `ch` chance of succeeding.
+ * @param {number} ch The chance.
+ * @example chance(0.1) // 10% chance of returning true
+ * @returns {boolean} If succeeded
+ */
 function chance(ch) {
 	return Math.random() < ch;
 }
+
+/**
+ * Returns a random value from the array.
+ * @param {any[]} array The array to choose from.
+ * @returns {any} The result.
+ */
 function randomArray(array) {
 	return array[Math.floor(Math.random() * array.length)];
 }
-function weightedChoose(object) {
-	/*
-		guide:
-		weightedChoose(
-			{
-				'thing': 2,
-				'egg': 5,
-				'h': 1,
-			}
-		)
-		this will make it so egg has the biggest chance of getting selected and h has the least chance of getting selected
-		an element has a value in sum of values of the object chance of getting selected
-		the sum of values is 2+5+1 = 8
-		so thing has a 2 in 8 (1 in 4) chance of getting selected (25%), egg has a 5 in 8 chance (62.5%)
-		and h has a 1 in 8 chance of getting selected (12.5%)
 
-		how it works: each element of the object gets added to an array the number of times in the number
-		then a random value from the array is chosen
-	*/
+/**
+ * @example
+ * weightedChoose(
+ * 	{
+ * 		'thing': 2,
+ * 		'egg': 5,
+ * 		'h': 1,
+ * 	}
+ * )
+ * 
+ * // this will make it so egg has the biggest chance of getting selected and h has the least chance of getting selected
+ * // an element has a value in sum of values of the object chance of getting selected
+ * // the sum of values is 2+5+1 = 8
+ * // so thing has a 2 in 8 (1 in 4) chance of getting selected (25%), egg has a 5 in 8 chance (62.5%) 
+ * // and h has a 1 in 8 chance of getting selected (12.5%)
+ * 
+ * // how it works: each element of the object gets added to an array the number of times in the number
+ * // then a random value from the array is chosen
+*/
+function weightedChoose(object) {
 
 	let array = [];
 	for (const value in object) {
@@ -40,6 +54,7 @@ function weightedChoose(object) {
 
 	return randomArray(array);
 }
+
 const xtoperc  = v => v / width;
 const ytoperc  = v => v / height;
 const xtopixel = v => v * width;
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 	the main code of the game, primarily drawing to the canvas
 */
 
-const canvas = document.getElementById('game');
+const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 var width = canvas.width;
@@ -14,6 +14,12 @@ timer = Math.floor(timerfull*100)/100;
 
 let l, r, jump, resetPending, nextLevelPending; // abbreviations for left and right
 
+function setup() {
+	tick(); // so the game won't randomly be bigger for a frame 
+	setInterval(tick, 100/6);
+	init();
+}
+
 // every frame, runs 60 fps, for technical stuff
 function tick() {
 	width = canvas.width = window.innerWidth;
@@ -22,16 +28,9 @@ function tick() {
 	if (width > height * 2) width = canvas.width = height * 2;
 	else height = canvas.height = width / 2;
 
-	// This sends index.html the required data to display the player's current level. ~ Quitin
-	document.getElementById('level').innerHTML = player.level + 1 // + 1 added to show Level 1 instead of 0 ~ Quitin
-	// Same as above, but with the timer. ~ Quitin
-	document.getElementById('timer').innerHTML = timer
-
 	timerfull = timerfull + 1/60;
 	timer = Math.floor(timerfull*100)/100;
 }
-
-setInterval(tick, 100/6);
 
 const player = {
 	x: 2,
@@ -47,282 +46,34 @@ const player = {
 
 const tileSize = 0.05;
 
-const images = {
-	man: new Image(),
-	block: new Image(),
-	exit: new Image(),
-	spike: new Image(),
-	checkpoint: new Image(),
-	checkpointactive: new Image(),
+function newImage(src) {
+    var img = new Image();
+    img.src = src;
+    return img;
 }
-images.man.src = `media/images/man.png`;
-images.block.src = `media/images/block.png`;
-images.exit.src = `media/images/exit.png`;
-images.spike.src = `media/images/spike.png`;
-images.checkpoint.src = `media/images/checkpoint.png`;
-images.checkpointactive.src = `media/images/checkpointactive.png`;
 
-// every frame, for rendering stuff
-function init() {
-	ctx.clearRect(0, 0, xtopixel(1), ytopixel(1));
+const images = {
+	// MAIN
+	man: newImage(`media/tiles/man.png`),
+	logo: newImage(`media/logo.png`),
 
-	// background
-	ctx.fillStyle = '#486362';
-	ctx.fillRect(0, 0, xtopixel(1), ytopixel(1));
+	// BACKGROUND
+	menu: newImage(`media/menu.png`),
 
-	const level = parseLevel();
+	// TILES
+	block: newImage(`media/tiles/block.png`),
+	exit: newImage(`media/tiles/exit.png`),
+	spike: newImage(`media/tiles/spike.png`),
+	checkpoint: newImage(`media/tiles/checkpoint.png`),
+	checkpointactive: newImage(`media/tiles/checkpointactive.png`),
+}
 
-	let spawnX = 4;
-	let spawnY = 4;
-
-	let Xscroll = 0;
-	let Yscroll = 0;
-
-	let checkpoints = [{ x: 4, y: 4 }];
-
-	const hitboxes = [];
-
-	if (LEVELS[player.level].xScroll) {
-		if (LEVELS[player.level].tiles[0].length > 1 / tileSize) {
-			if (player.x >= 0.5 / tileSize && player.x <= LEVELS[player.level].tiles[0].length - 0.5 / tileSize) {
-				Xscroll = player.x - 0.5 / tileSize;
-			}
-			else if (player.x <= 0.5 / tileSize) {
-				Xscroll = 0;
-			}
-			else {
-				Xscroll = LEVELS[player.level].tiles[0].length - 1 / tileSize;
-			}
-		}
-	}
-	if (LEVELS[player.level].yScroll) {
-		if (LEVELS[player.level].tiles.length > 0.5 / tileSize) {
-			if (player.y >= 0.25 / tileSize && player.y <= LEVELS[player.level].tiles.length - 0.25 / tileSize) {
-				Yscroll = player.y - 0.25 / tileSize;
-			}
-			else if (player.y <= 0.25 / tileSize) {
-				Yscroll = 0;
-			}
-			else {
-				Yscroll = LEVELS[player.level].tiles.length - 0.5 / tileSize;
-			}
-		}
-	}
-
-	// render the level
-	for (let y = 1; y < LEVELS[player.level].tiles.length + 1; y++) {
-		for (let x = 1; x < getWidth(LEVELS[player.level].tiles) + 1; x++) {
-			if (level[0] == undefined) {
-				level[0] = {
-					x: x,
-					y: y,
-					tile: 'block',
-				}
-			}
-			if (level[0].x == x && level[0].y == y) {
-				if (level[0].tile == 'spawn') {
-					spawnX = level[0].x;
-					spawnY = level[0].y;
-					checkpoints[0].x = spawnX;
-					checkpoints[0].y = spawnY;
-					level.shift();
-					continue;
-				}
-				if (level[0].tile == 'checkpoint') {
-					checkpoints.push({
-						x: x,
-						y: y,
-					});
-				}
-				let texture = images[level[0].tile];
-				if (level[0].tile == 'checkpoint' && player.checkpoint == checkpoints.length - 1) texture = images.checkpointactive;
-
-				ctx.drawImage(texture, (x - 1 - Xscroll) * xtopixel(tileSize), (y - 1 - Yscroll) * xtopixel(tileSize), xtopixel(tileSize), xtopixel(tileSize));
-
-				let tDecr = 0;
-				if (level[0].tile == 'spike') tDecr = 0.15;
-
-				if (hasHitbox(level[0].tile)) {
-					if (level[0].tile == 'checkpoint') {
-						hitboxes.push({
-							tile: level[0].tile,
-							top: (y - 1 + tDecr) * tileSize * 20,
-							left: (x - 1) * tileSize * 20,
-							bottom: ((y - 1) * tileSize + tileSize) * 20,
-							right: ((x - 1) * tileSize + tileSize) * 20,
-							checkpoint: checkpoints.length - 1,
-						});
-					}
-					else {
-						hitboxes.push({
-							tile: level[0].tile,
-							top: (y - 1 + tDecr) * tileSize * 20,
-							left: (x - 1) * tileSize * 20,
-							bottom: ((y - 1) * tileSize + tileSize) * 20,
-							right: ((x - 1) * tileSize + tileSize) * 20,
-						});
-					}
-				}
-
-				level.shift();
-			}
-		}
-	}
-	for (const value of hitboxes) {
-		// making sure there are no rounding errors
-		value.top = Math.round((value.top + Number.EPSILON) * 1000) / 1000;
-		value.left = Math.round((value.left + Number.EPSILON) * 1000) / 1000;
-		value.bottom = Math.round((value.bottom + Number.EPSILON) * 1000) / 1000;
-		value.right = Math.round((value.right + Number.EPSILON) * 1000) / 1000;
-	}
-
-	if (!player.spawned) {
-		player.x = checkpoints[player.checkpoint].x + 0.125;
-		player.y = checkpoints[player.checkpoint].y + 0.25;
-		player.spawned = true;
-	}
-
-
-	let friction = 0.99;
-	let velThreshold = 0.8;
-
-	if (l) player.vx -= player.speed;
-	if (r) player.vx += player.speed;
-	if (jump) {
-		jump = false;
-		if (player.canJump) {
-			player.vy = -20;
-			player.canJump = false;
-		}
-	}
-
-	if (nextLevelPending) {
-		reset();
-		nextLevelPending = false;
-	}
-
-	gravitate();
-	move();
-
-	if (player.vx > 0 && l) friction = 0.9;
-	if (player.vx < 0 && r) friction = 0.9;
-	if (!l && !r) friction = 0.875;
-	if (player.vx > -velThreshold && player.vx < velThreshold && !l && !r) friction = 0.81;
-	if (player.vx > -velThreshold && player.vx < 0 && l && !r) friction += 0.2;
-	if (player.vx < velThreshold && player.vx > 0 && r && !l) friction += 0.2;
-	friction -= Math.abs(player.vx) / 2000;
-
-	player.vx *= friction;
-
-	if (resetPending) {
-		if (player.checkpoint == 0) {
-			timerfull = 0;
-		}
-		reset();
-		resetPending = false;
-	}
-
-	// making sure there are no rounding errors
-	player.x = Math.round((player.x + Number.EPSILON) * 1000) / 1000;
-	player.y = Math.round((player.y + Number.EPSILON) * 1000) / 1000;
-
-	ctx.drawImage(images.man, ((player.x - 1 - Xscroll) * xtopixel(tileSize)), ((player.y - 1 - Yscroll) * xtopixel(tileSize)), xtopixel(tileSize * 0.75), xtopixel(tileSize * 0.75));
-
-	requestAnimationFrame(init);
-
-
-	function move() {
-		let dir;
-		let temp = player.x;
-		let tempcollide = colliding();
-
-		player.x += player.vx / 150;
-		if (player.x > temp) dir = 'right';
-		if (player.x < temp) dir = 'left';
-		if (player.x == temp) dir = 'none';
-
-		let collide = colliding();
-		if (collide) {
-			let sp = specialCollide(collide);
-			if (!sp) {
-				if (dir == 'right') {
-					player.x = collide.left + 0.25;
-					player.vx = 0;
-				}
-				if (dir == 'left') {
-					player.x = collide.right + 1;
-					player.vx = 0;
-				}
-			}
-		}
-
-		
-		temp = player.y;
-		tempcollide = colliding();
-
-		player.y += player.vy / 150;
-		if (player.y > temp) dir = 'down';
-		if (player.y < temp) dir = 'up';
-		if (player.y == temp) dir = 'none';
-
-		collide = colliding();
-		if (collide) {
-			sp = specialCollide(collide);
-			if (!sp) {
-				if (dir == 'down') {
-					player.y = collide.top + 0.25;
-					player.vy = 0;
-					player.canJump = true;
-				}
-				if (dir == 'up') {
-					player.y = collide.bottom + 1;
-					player.vy = 0;
-				}
-			}
-		}
-		if (!collide) {
-			player.canJump = false;
-		}
-	}
-	function gravitate() {
-		player.vy += 0.5;
-	}
-	function colliding() {
-		let sfl = undefined; // save for later
-		for (const box of hitboxes) {
-			if (player.x > box.left + 0.25 && player.x - 1 < box.right && player.y > box.top + 0.25 && player.y - 1 < box.bottom) {
-				if (hitboxNoSolid(box.tile)) {
-					sfl = box;
-					continue;
-				}
-				return box;
-			}
-		}
-		if (sfl) return sfl;
-		return false;
-	}
-	function reset() {
-		player.x = checkpoints[player.checkpoint].x + 0.125;
-		player.y = checkpoints[player.checkpoint].y + 0.25;
-		player.vx = 0;
-		player.vy = 0;
-	}
-	function specialCollide(prop) { // returns true if no hitbox
-		if (prop.tile == 'spike') {
-			reset();
-			return true;
-		}
-		if (prop.tile == 'exit' && player.level != LEVELS.length - 1) {
-			player.checkpoint = 0;
-			player.level++;
-			nextLevelPending = true;
-			timerfull = 0;
-		}
-		if (prop.tile == 'checkpoint') {
-			player.checkpoint = prop.checkpoint;
-			return true;
-		}
-	}
+/**
+ */
+const state = {
+	playing: false,
+	tab: 'menu',
+	subtab: 'main',
 }
 
 function keyDownHandler(event) {
